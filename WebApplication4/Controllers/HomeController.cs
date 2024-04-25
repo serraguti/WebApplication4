de0ec681bc5850b6861fa77f68a6f595efc1584f
serraguti <pacoserranox@gmail.com>
2024-04-25 15:11:20 +0200

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        //NECESITAMOS INYECTAR SECRETCLIENT
+        private SecretClient secretClient;
+
+        public HomeController(SecretClient secretClient)
+        {
+            this.secretClient = secretClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string secretkey)
+        {
+            KeyVaultSecret secret =
+                await this.secretClient.GetSecretAsync(secretkey);
+            ViewData["SECRETO"] = secret.Value;
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
